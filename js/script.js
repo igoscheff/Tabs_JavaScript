d79db702290787000cf20efedc36a8f40590d6ac
Dimitri Igoshev <igoscheff@gmail.com>
2020-02-13 10:49:14 +0100

    yoga

---
+++ js/script.js
@@ -0,0 +1,39 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+    'use strict'
+
+    let tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent')
+
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show')
+            tabContent[i].classList.add('hide')
+        }
+    }
+
+    hideTabContent(1)
+
+    function showTabContent(a) {
+        if (tabContent[a].classList.contains('hide')) {
+            tabContent[a].classList.remove('hide')
+            tabContent[a].classList.add('show')
+        }
+    }
+
+    info.addEventListener('click', function(event) {
+        let target = event.target
+
+        if (target && target.classList.contains('info-header-tab')) {
+            for (let i = 0; tab.length; i++) {
+                if (target == tab[i]) {
+                    hideTabContent(0)
+                    showTabContent(i)
+                    break;
+                }
+            }
+        }
+    })
+
+})
