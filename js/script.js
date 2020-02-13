1e1a4edd8921a0fad9273df8d3e97cc5e3e39cf0
Dimitri Igoshev <igoscheff@gmail.com>
2020-02-13 16:17:36 +0100

    timer

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ window.addEventListener('DOMContentLoaded', function() {
 
     'use strict'
 
+    ///////////////////////// Табы /////////////////////////
+
     //Получение объектов со страницы (табы, родитель табов, контент табов)
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
@@ -40,5 +42,51 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     })
+
+    ///////////////////////// Таймер /////////////////////////
+
+    //Дата окончания таймера
+    let deadline = '2020-02-13';
+
+    //Функция вазвращающая интервал до окончания таймера
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+        seconds = Math.floor((t/1000) % 60),
+        minutes = Math.floor((t/1000/60) % 60),
+        hours = Math.floor(t/1000/60/60);
+
+        return {
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        }
+    }
+
+    //Функция установки и обнавления времени
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            hours.textContent = t.hours < 10 ? '0' + t.hours : t.hours;
+            minutes.textContent = t.minutes < 10 ? '0' + t.minutes : t.minutes;
+            seconds.textContent = t.seconds < 10 ? '0' + t.seconds : t.seconds;
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval)
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+    }
+
+    //Вызов функции установки и обнавления времени
+    setClock('timer', deadline)
 
 })
